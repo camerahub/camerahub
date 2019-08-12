d3b8e42c27d58f7302f1dbc99a7b22fed908be14
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2019-08-12 23:23:35 +0100

    Create Manufacturer model

--- schema/models.py
+++ schema/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Manufacturer(models.Model):
+  manufacturer = models.CharField('Name of the manufacturer', max_length=45, unique=True)
+  city = models.CharField('City in which the manufacturer is based', max_length=45)
+  country = models.CharField('Country in which the manufacturer is based', max_length=45)
+  url = models.URLField('URL to the manufacturers main website', max_length=45)
+  founded = models.DateField('Year in which the manufacturer was founded')
+  dissolved = models.DateField('Year in which the manufacturer was dissolved')
+
