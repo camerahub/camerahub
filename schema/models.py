581277fe5b2ae19475f30b8abad7669a778e409a
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2019-08-12 23:53:36 +0100

    Add Accessory and AccessoryType models

--- schema/models.py
+++ schema/models.py
@@ -9,3 +9,17 @@ class Manufacturer(models.Model):
   founded = models.DateField('Year in which the manufacturer was founded')
   dissolved = models.DateField('Year in which the manufacturer was dissolved')
 
+# Table to list types of accessories
+class AccessoryType(models.Model):
+  type = models.CharField('Type of accessory', max_length=45, unique=True)
+
+# Table to catalog accessories that are not tracked in more specific tables
+class Accessory(models.Model):
+  type = models.ForeignKey(AccessoryType, on_delete=models.CASCADE)
+  manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+  model = models.CharField('Model of the accessory', max_length=45)
+  acquired = models.DateField('Date that this accessory was acquired')
+  cost = models.DecimalField('Purchase cost of the accessory', max_digits=6, decimal_places=2)
+  lost = models.DateField('Date that this accessory was lost')
+  lost_price = models.DecimalField('Sale price of the accessory', max_digits=6, decimal_places=2)
+
