8230e6c8711bae1a5b15f7536a4f20937df8bdec
Jonathan Gazeley <me@jonathangazeley.com>
2019-08-13 15:02:45 +0100

    Bare minimum required to use admin editor

--- schema/admin.py
+++ schema/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Manufacturer
+admin.site.register(Manufacturer)
+
+from .models import CameraModel
+admin.site.register(CameraModel)
