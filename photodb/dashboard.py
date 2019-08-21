0e99c62503572f0bdf10613ca94a965ce6dd27f0
Jonathan Gazeley <me@jonathangazeley.com>
2019-08-21 14:25:06 +0100

    Drop feed applet

--- photodb/dashboard.py
+++ photodb/dashboard.py
@@ -56,13 +56,6 @@ class CustomIndexDashboard(Dashboard):
         # append a recent actions module
         self.children.append(modules.RecentActions(_('Recent Actions'), 5))
 
-        # append a feed module
-        self.children.append(modules.Feed(
-            _('Latest Django News'),
-            feed_url='http://www.djangoproject.com/rss/weblog/',
-            limit=5
-        ))
-
         # append another link list module for "support".
         self.children.append(modules.LinkList(
             _('Support'),
