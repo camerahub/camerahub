352f8a028683f2d3279994dd01db5c3a311e222f
Jonathan <me@jonathangazeley.com>
2019-10-02 14:45:58 +0100

    SQLite Docker volume persistence (#94)
    Move default location of the SQLite db file into a subdir and mount this subdir as a named volume, so its contents are persistent

--- photodb/settings.py
+++ photodb/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'photodb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
