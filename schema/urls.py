ffdf52f3e52c913ba3e5c0b146dcbf9fbc1b754f
Jonathan Gazeley <me@jonathangazeley.com>
2019-10-15 22:37:11 +0100

    Barebones implementation of list anad detail views

--- schema/urls.py
+++ schema/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+  path('', views.index, name='index'),
+  path('cameras/', views.CameraListView.as_view(), name='cameras'),
+  path('cameras/<int:pk>', views.CameraDetailView.as_view(), name='cameras'),
 ]
--- schema/views.py
+++ schema/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 def index(request):
     return render(request, 'schema/index.html')
+
+from .models import Camera
+class CameraListView(generic.ListView):
+  model = Camera
+  template_name = 'schema/list.html'
+
+class CameraDetailView(generic.DetailView):
+  model = Camera
+  template_name = 'schema/detail.html'
