a00a99f2ab5054276a726ef4723f050fc356757a
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2019-10-15 19:11:33 +0100

    Switch to use trivial template to welcome pag

--- schema/views.py
+++ schema/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the schema index.")
+    return render(request, 'schema/index.html')
