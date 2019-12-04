cc9d2f47a3a46c8ae3127c496c312b065484b585
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2019-12-04 10:37:32 +0000

    Improve icon script to make multiple PNG from one SVG and use Inkscape due to bugs in ImageMagick

--- icons/generate-icons.pl
+++ icons/generate-icons.pl
@@ -6,41 +6,42 @@
 use strict;
 use warnings;
 
-# Set up mappings between old and new icon names
-my %newname = (
-	'icons8-camera-addon'		=> 'accessory',
-	'icons8-collectibles'		=> 'archive',
-	'icons8-battery'		=> 'battery',
-	'icons8-film-reel'		=> 'bulkfilm',
-	'icons8-cameras'		=> 'cameramodel',
-	'icons8-camera'			=> 'camera',
-	'icons8-camera-identification'	=> 'condition',
-	'icons8-volume'			=> 'developer',
-	'icons8-industrial-scales'	=> 'enlarger',
-	'icons8-easel'			=> 'exhibition',
-	'icons8-medium-icons'		=> 'exposureprogram',
-	'icons8-film-roll'		=> 'film',
-	'icons8-360-degrees'		=> 'filter',
-	'icons8-camera-flash'		=> 'flash',
-	'icons8-connected-no-data'	=> 'flashprotocol',
-	'icons8-resolution'		=> 'format',
-	'icons8-lens'			=> 'lens',
-	'icons8-factory'		=> 'manufacturer',
-	'icons8-slr-small-lens'		=> 'mount',
-	'icons8-metamorphose'		=> 'mountadapter',
-	'icons8-movie'			=> 'negative',
-	'icons8-surface'		=> 'negativesize',
-	'icons8-transaction-list'	=> 'order',
-	'icons8-sheets'			=> 'paperstock',
-	'icons8-user'			=> 'person',
-	'icons8-xlarge-icons'		=> 'print',
-	'icons8-lab-items'		=> 'process',
-	'icons8-screwdriver'		=> 'repair',
-	'icons8-jpg'			=> 'scan',
-	'icons8-diversity'		=> 'series',
-	'icons8-exposure'		=> 'shutterspeed',
-	'icons8-small-lens'		=> 'teleconverter',
-	'icons8-test-tube'		=> 'toner',
+# Set up mappings between old and new icon names. One SVG can be mapped to multiple PNGs
+my %newnames = (
+	'icons8-camera-addon'			=> ['accessory'],
+	'icons8-collectibles'			=> ['archive'],
+	'icons8-battery'				=> ['battery'],
+	'icons8-film-reel'				=> ['bulkfilm'],
+	'icons8-cameras'				=> ['cameramodel'],
+	'icons8-camera'					=> ['camera'],
+	'icons8-camera-identification'	=> ['condition'],
+	'icons8-volume'					=> ['developer'],
+	'icons8-industrial-scales'		=> ['enlarger'],
+	'icons8-easel'					=> ['exhibition'],
+	'icons8-medium-icons'			=> ['exposureprogram'],
+	'icons8-film-roll'				=> ['film', 'filmstock'],
+	'icons8-360-degrees'			=> ['filter'],
+	'icons8-camera-flash'			=> ['flash'],
+	'icons8-connected-no-data'		=> ['flashprotocol'],
+	'icons8-resolution'				=> ['format'],
+	'icons8-lens'					=> ['lens', 'lensmodel'],
+	'icons8-factory'				=> ['manufacturer'],
+	'icons8-slr-small-lens'			=> ['mount'],
+	'icons8-metamorphose'			=> ['mountadapter'],
+	'icons8-movie'					=> ['negative'],
+	'icons8-surface'				=> ['negativesize'],
+	'icons8-transaction-list'		=> ['order'],
+	'icons8-sheets'					=> ['paperstock'],
+	'icons8-user'					=> ['person'],
+	'icons8-xlarge-icons'			=> ['print'],
+	'icons8-lab-items'				=> ['process'],
+	'icons8-screwdriver'			=> ['repair'],
+	'icons8-jpg'					=> ['scan'],
+	'icons8-diversity'				=> ['series'],
+	'icons8-exposure'				=> ['shutterspeed'],
+	'icons8-small-lens'				=> ['teleconverter'],
+	'icons8-test-tube'				=> ['toner'],
+	'icons8-home'					=> ['home'],
 #unknown
 #login
 #logout
@@ -67,8 +68,11 @@ for my $resolution (@resolutions) {
 		$file =~ m/^svg\/(.+)-\d+\.svg$/;
 		
 		# If a mapping exists	
-		if ($newname{$1}) {
-			`convert -size ${resolution}x${resolution} $file $output/$resolution/$newname{$1}${resolution}.png`;
+		if ($newnames{$1}) {
+			foreach my $newname (@{$newnames{$1}}) {
+				# Generate a PNG for each hash value
+				`inkscape -z -e $output/$resolution/$newname${resolution}.png -w $resolution -h $resolution $file`
+			}
 		}
 	}
 }
