a650597c5d969d91f12dc3a7eaabb0f41f50d356
Jonathan Gazeley <me@jonathangazeley.com>
2020-01-14 22:00:48 +0000

    Capture user email at signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.urls import reverse_lazy
 from django.views import generic
 
-
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
