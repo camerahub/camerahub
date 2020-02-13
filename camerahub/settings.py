c588edd8b0bb362f4a4a8943b2a23a34ebefe625
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2020-02-13 10:25:38 +0000

    Fix typo in settings

--- camerahub/settings.py
+++ camerahub/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'schema.apps.SchemaConfig',
+    'schema',
     'djmoney',
     'favicon',
     'django_tables2',
