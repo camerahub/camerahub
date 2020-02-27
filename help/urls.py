6ce69b8f57fff011a786596fd28532c67ab37f3c
Jonathan Gazeley <me@jonathangazeley.com>
2020-02-27 21:25:16 +0000

    Move about page into help

--- help/urls.py
+++ help/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # Static pages
     path('', TemplateView.as_view(template_name='index.html'), name='help'),
     path('concepts', TemplateView.as_view(template_name='concepts.html'), name='concepts'),
+    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
--- schema/urls.py
+++ schema/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 
   # Static pages
   path('', TemplateView.as_view(template_name='schema/index.html'), name='index'),
-  path('about', TemplateView.as_view(template_name='schema/about.html'), name='about'),
   path('stats', views.StatsView.as_view(), name='stats'),
 
   path('accessory/', views.AccessoryList.as_view(), name='accessory-list'),
