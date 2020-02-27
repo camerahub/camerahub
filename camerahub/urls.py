47c7d64eac86a3776541794a78a45129a5bfc1c5
Jonathan Gazeley <me@jonathangazeley.com>
2020-02-27 20:57:17 +0000

    Add skeleton help page

--- camerahub/urls.py
+++ camerahub/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('schema.urls')),
+    path('help', include('help.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
---
+++ help/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+
+    # Static pages
+    path('', TemplateView.as_view(template_name='index.html'), name='help'),
+    path('concepts', TemplateView.as_view(template_name='concepts.html'), name='concepts'),
+]
