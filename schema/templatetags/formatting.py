31f0f65ab532833a6b998611645ac2b22d56d13c
Jonathan Gazeley <me@jonathangazeley.com>
2020-08-18 19:54:10 +0100

    Format shutter speeds with a superscript 1

--- schema/templatetags/formatting.py
+++ schema/templatetags/formatting.py
@@ -3,7 +3,15 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.filter(is_safe=True)
 def aperture(obj):
     newstr = mark_safe(obj.replace('f/', '<em>f</em>/'))
+    return newstr
+
+
+@register.filter(is_safe=True)
+def shutter(obj):
+    mystr = str(obj)
+    newstr = mark_safe(mystr.replace('1/', '&sup1;/'))
     return newstr
