acaafa42abca7a57acb701cca6dc93056d590fa7
Jonathan Gazeley <me@jonathangazeley.com>
2020-11-10 20:55:49 +0000

    Revert "Rename api URLs so they don't clash with the regular URLs"
    This reverts commit 6ee433ba8f9369281df6ea8b1cb6af2fe6425071.

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from api import views
 
 # API endpoints
 router = routers.DefaultRouter()
-router.register(r'film', views.FilmViewSet, 'apifilm')
-router.register(r'negative', views.NegativeViewSet, 'apinegative')
-router.register(r'scan', views.ScanViewSet, 'apiscan')
-router.register(r'print', views.PrintViewSet, 'apiprint')
+router.register(r'film', views.FilmViewSet)
+router.register(r'negative', views.NegativeViewSet)
+router.register(r'scan', views.ScanViewSet)
+router.register(r'print', views.PrintViewSet)
 
 urlpatterns = [
     # Wire up our API using automatic URL routing.
