a6baf0dedd0fd6823ef0961ea6a3a9b4b50cb709
Jonathan Gazeley <me@jonathangazeley.com>
2020-12-02 20:36:05 +0000

    Specify non-atomic renames

--- schema/migrations/0105_auto_20201117_2146.py
+++ schema/migrations/0105_auto_20201117_2146.py
@@ -5,6 +5,7 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
+    atomic = False
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
