e33d52284558e0a91e11add83ddc3ca0e17caac1
Jonathan Gazeley <me@jonathangazeley.com>
2020-12-02 22:18:02 +0000

    Tweak admin headings

--- schema/admin.py
+++ schema/admin.py
@@ -9,13 +9,13 @@ from schema.models import Accessory, Archive, Battery, BulkFilm, Camera, CameraM
 admin.site.site_header = 'CameraHub'
 
 # The text to put at the end of each admin page’s <title> (a string). By default, this is “Django site admin”.
-admin.site.site_title = 'CameraHub'
+admin.site.site_title = 'CameraHub admin'
 
 # The text to put at the top of the admin index page (a string). By default, this is “Site administration”.
-admin.site.index_title = 'CameraHub'
+admin.site.index_title = 'CameraHub admin'
 
 # The URL for the “View site” link at the top of each admin page. By default, site_url is /. Set it to None to remove the link.
-admin.site.site_url = None
+#admin.site.site_url = None
 
 # Now register the admin pages, customising where necessary
 admin.site.register(Accessory)
