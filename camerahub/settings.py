4feede7f6eaa8dae20a9987b3b8c989e5594d2b7
Jonathan Gazeley <me@jonathangazeley.com>
2020-12-18 19:47:58 +0000

    Use en-GB localisation

--- camerahub/settings.py
+++ camerahub/settings.py
@@ -140,7 +140,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
