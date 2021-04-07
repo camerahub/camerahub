f9f817ae60b2e30af01207304025ccb9fb8109bd
Jonathan Gazeley <me@jonathangazeley.com>
2021-04-07 22:39:00 +0100

    Clear static files before populating

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
     python manage.py installwatson
     python manage.py buildwatson
     python manage.py populate_history --auto
-    python manage.py collectstatic
+    python manage.py collectstatic -c --noinput
 fi
 
 exec "$@"
