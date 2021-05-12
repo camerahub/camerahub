ecf6b7b80827bc18fc27a22398952abcf9c46d3d
Jonathan Gazeley <me@jonathangazeley.com>
2021-05-12 22:43:40 +0100

    Drop links to concepts

--- help/urls.py
+++ help/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 
     # Static pages
     path('', TemplateView.as_view(template_name='index.html'), name='help'),
-    path('concepts', TemplateView.as_view(template_name='concepts.html'), name='concepts'),
     path('about', TemplateView.as_view(template_name='about.html'), name='about'),
     path('faq', TemplateView.as_view(template_name='faq.html'), name='faq'),
     path('condition', ConditionListView.as_view(), name='condition-list'),
