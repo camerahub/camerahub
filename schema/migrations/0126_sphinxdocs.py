1925ceb6277c4181ff2ff1e6d2c97d4c89f26897
Jonathan Gazeley <me@jonathangazeley.com>
2021-05-20 22:22:48 +0100

    Automatically se up Sphinx and build docs

---
+++ schema/migrations/0126_sphinxdocs.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('schema', '0125_auto_20210517_2102'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql=[("INSERT INTO sphinxdoc_project (id, name, slug, path) VALUES (1, 'CameraHub', 'camerahub', 'docs');")],
+            reverse_sql=[("DELETE FROM sphinxdoc_project where id=1;")],
+        )
+    ]
