abcc2f1de35df72bed4d6b2f79080ba4ad963a80
Jonathan Gazeley <me@jonathangazeley.com>
2022-05-24 21:29:16 +0100

    Drop collectionfield

--- schema/models.py
+++ schema/models.py
@@ -17,7 +17,6 @@ from taggit.managers import TaggableManager
 from django_prometheus.models import ExportModelOperationsMixin
 from simple_history.models import HistoricalRecords
 from versatileimagefield.fields import VersatileImageField
-from collectionfield.models import CollectionField
 from django_countries.fields import CountryField
 from geoposition.fields import GeopositionField
 from star_ratings.models import Rating
@@ -1612,8 +1611,6 @@ class CameraModel(ExportModelOperationsMixin('cameramodel'), models.Model):
         Manufacturer, on_delete=models.CASCADE, help_text='Manufacturer of this camera model', verbose_name='manufacturer')
     model = models.CharField(
         help_text='The model name of the camera', max_length=45)
-    other_names = CollectionField(
-        help_text='Other model names that this camera may be known by, e.g. in other parts of the world. Separate with commas.', blank=True, null=True)
     disambiguation = models.CharField(
         help_text='Distinguishing notes for camera models with the same name', max_length=45, blank=True, default='')
     mount = models.ForeignKey(Mount, on_delete=models.CASCADE, blank=True, null=True,
