23dfacea0ec98ec70d9285bb13f95b21407dde16
Jonathan Gazeley <me@jonathangazeley.com>
2022-06-05 20:28:17 +0100

    Trivial setup of iommi

---
+++ ui/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from iommi import Form, Table
+from schema import models
+
+urlpatterns = [
+    path('battery/create/', Form.create(auto__model=models.Battery).as_view()),
+    path('battery/', Table(auto__model=models.Battery).as_view()),
+
+    path('manufacturer/create/', Form.create(auto__model=models.Manufacturer).as_view()),
+    path('manufacturer/', Table(auto__model=models.Manufacturer).as_view()),
+]
