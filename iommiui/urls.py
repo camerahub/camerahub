109b824714bf91329a39434846876ed99f532936
Jonathan Gazeley <me@jonathangazeley.com>
2023-05-20 21:08:38 +0100

    Workaround for iommi + taggit

--- iommiui/urls.py
+++ iommiui/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+
 from iommi import Table
+from iommi import register_factory
+
+from taggit.managers import TaggableManager
+
 
 # Import any models you need from your models.  Here I'm using Album
 from schema.models import Manufacturer
+
+# Workaround for https://github.com/iommirocks/iommi/issues/339
+register_factory(GenericRelation, factory=None)
+register_factory(GenericForeignKey, factory=None)
+register_factory(TaggableManager, shortcut_name='many_to_many')
 
 urlpatterns = [
     # ...your urls...
