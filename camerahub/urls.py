a12fb5157d30fed235d3de6d27c3756ecd9ab992
Jonathan Gazeley <me@jonathangazeley.com>
2023-05-20 21:16:30 +0100

    Rename iommiui to ui

--- camerahub/urls.py
+++ camerahub/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('schema.urls')),
     path('api/', include('api.urls')),
-    path('ui/', include('iommiui.urls')),
+    path('ui/', include('ui.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
--- ui/apps.py
+++ ui/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IommiuiConfig(AppConfig):
+class UiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'iommiui'
+    name = 'ui'
