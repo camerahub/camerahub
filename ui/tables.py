2e8d67ddf923816b7d154d7e15fdd66941b7de31
Jonathan Gazeley <me@jonathangazeley.com>
2023-06-13 20:45:37 +0100

    Work on manufacturer list view

--- ui/tables.py
+++ ui/tables.py
@@ -1,10 +1,13 @@
 from iommi import Table, Form, Column, Action
+from django.utils.html import format_html
 from ui.pages import BasePage
 from schema import models
 
 class ManufacturerListPage(BasePage):
     table = Table(
         auto__model=models.Manufacturer,
+        columns__name__cell__url=lambda row, **_: row.get_absolute_url(),
+        columns__country__cell__format=lambda row, **_: format_html("{} <img src=\"{}\">", row.country.name, row.country.flag),
         query_from_indexes=True,
         actions__create_manufacturer=Action(
             attrs__href='/ui/manufacturer/create/',
