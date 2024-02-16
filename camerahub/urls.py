5fc25b84a40c24bfece33e4fe41821153a3b395b
Jonathan Gazeley <me@jonathangazeley.com>
2024-02-16 22:01:41 +0000

    Rebase configs to Django 4.2

--- camerahub/urls.py
+++ camerahub/urls.py
@@ -1,7 +1,8 @@
-"""camerahub URL Configuration
+"""
+URL configuration for camerahub project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('schema.urls')),
